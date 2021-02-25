bc8f3fb3a67627d305441d3e424b173095c5e1a8
Young wook Jeon <ela87@naver.com>
2021-02-25 22:07:30 +0900

    Add signUp page and style it

---
+++ signUp.php
@@ -0,0 +1,41 @@
+<?php require_once("includes/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VideoTube</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
+</head>
+<body>
+    <div class="signInContainer">
+        <div class="column">
+            <div class="header">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
+                <h3>Sign Up</h3>
+                <span>to continue to VideoTube</span>
+            </div>
+
+            <div class="loginForm">
+                <form action="signUp.php" method="POST">
+                    <input type="text" name="firstName" placeholder="First name" autocomplete="off" required>
+                    <input type="text" name="lastName" placeholder="Last name" autocomplete="off" required>
+                    <input type="text" name="username" placeholder="Username" autocomplete="off" required>
+                    <input type="email" name="email" placeholder="Email" autocomplete="off" required>
+                    <input type="email" name="email2" placeholder="Confirm Email" autocomplete="off" required>
+
+                    <input type="password" name="password" placeholder="Password" autocomplete="off" required>
+                    <input type="password" name="password2" placeholder="Confirm Password" autocomplete="off" required>
+
+                    <input type="submit" name="submitButton" value="SUBMIT">
+                </form>
+            </div>
+
+            <a class="signInMessage" href="signIn.php">Already have an account? Sign in here!</a>
+        </div>
+    </div>
+</body>
+</html>
