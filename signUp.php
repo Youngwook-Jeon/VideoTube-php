d9f8955f9302ef00038844d7580bed321a0992b4
Young wook Jeon <ela87@naver.com>
2021-02-25 23:50:19 +0900

    Validate users information

--- signUp.php
+++ signUp.php
@@ -1,4 +1,26 @@
-<?php require_once("includes/config.php"); ?>
+<?php 
+require_once("includes/config.php"); 
+require_once("includes/classes/Account.php"); 
+require_once("includes/classes/Constants.php"); 
+require_once("includes/classes/FormSanitizer.php"); 
+
+$account = new Account($con);
+
+if (isset($_POST["submitButton"])) {
+    $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+    $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+
+    $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+
+    $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+    $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
+
+    $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+    $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+
+    $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,8 +43,15 @@
 
             <div class="loginForm">
                 <form action="signUp.php" method="POST">
+
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
                     <input type="text" name="firstName" placeholder="First name" autocomplete="off" required>
+
+                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
                     <input type="text" name="lastName" placeholder="Last name" autocomplete="off" required>
+
+                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                    <?php echo $account->getError(Constants::$usernameTaken); ?>
                     <input type="text" name="username" placeholder="Username" autocomplete="off" required>
                     <input type="email" name="email" placeholder="Email" autocomplete="off" required>
                     <input type="email" name="email2" placeholder="Confirm Email" autocomplete="off" required>
