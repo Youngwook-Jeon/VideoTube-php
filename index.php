5243c0508e63746b49f64fcec5fe89fb4330f027
Young wook Jeon <ela87@naver.com>
2021-02-26 19:19:35 +0900

    Output the video player

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once("includes/header.php"); ?>
 <?php 
 if (isset($_SESSION["userLoggedIn"])) {
-    echo "User is logged in as " . $_SESSION["userLoggedIn"];
+    echo "User is logged in as.. " . $userLoggedInObj->getName();
 } else {
     echo "not logged in";
 }
