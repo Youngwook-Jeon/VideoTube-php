cbeba23510c927cc5785857bc20e5d05b0159984
Young wook Jeon <ela87@naver.com>
2021-02-28 23:26:33 +0900

    Like/dislike video action

---
+++ ajax/likeVideo.php
@@ -0,0 +1,13 @@
+<?php
+require_once("../includes/config.php"); 
+require_once("../includes/classes/Video.php"); 
+require_once("../includes/classes/User.php"); 
+
+$username = $_SESSION["userLoggedIn"];
+$videoId = $_POST["videoId"];
+
+$userLoggedIn = new User($con, $username);
+$video = new Video($con, $videoId, $userLoggedIn);
+
+echo $video->like();
+?>
